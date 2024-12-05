9d0525024b26037a749c95657be7691f8883d1a0
luoyunxin <luoyunxin@espressif.com>
2024-12-05 16:37:56 +0800

    first commit

---
+++ main/app_main.c
@@ -0,0 +1,35 @@
+#include <stdio.h>
+#include "freertos/FreeRTOS.h"
+#include "freertos/task.h"
+#include "nvs_flash.h"
+#include "nvs.h"
+#include "esp_log.h"
+#include "esp_err.h"
+#include "esp_check.h"
+#include "esp_memory_utils.h"
+#include "lvgl.h"
+#include "bsp/esp-bsp.h"
+#include "bsp/display.h"
+#include "ui.h"
+
+void app_main(void)
+{
+    bsp_display_cfg_t cfg = {
+        .lvgl_port_cfg = ESP_LVGL_PORT_INIT_CONFIG(),
+        .buffer_size = BSP_LCD_DRAW_BUFF_SIZE,
+        .double_buffer = BSP_LCD_DRAW_BUFF_DOUBLE,
+        .flags = {
+            .buff_dma = true,
+            .buff_spiram = false,
+            .sw_rotate = false,
+        }
+    };
+    bsp_display_start_with_config(&cfg);
+    bsp_display_backlight_on();
+
+    bsp_display_lock(0);
+
+    ui_init();
+
+    bsp_display_unlock();
+}
